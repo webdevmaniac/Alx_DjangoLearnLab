7eb4070a314367c6bfd2af83d76dc5b41deb5d7f
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-02-25 20:18:33 +0200

    Role based

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -26,3 +30,21 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Library
 from .models import Book, Author, Librarian
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
 
 @login_required
 def list_books(request):
@@ -44,3 +47,25 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+def member_view(request):
+    return render(request, 'member_view.html')
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+admin_view = user_passes_test(is_admin)(admin_view)
+librarian_view = user_passes_test(is_librarian)(librarian_view)
+member_view = user_passes_test(is_member)(member_view)
