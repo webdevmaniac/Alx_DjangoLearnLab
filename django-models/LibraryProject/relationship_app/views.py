870f47c4c03b0fab78a85f16ff8836a226b05d7d
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-02-25 15:06:20 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Library
 from .models import Book, Author, Librarian
 
@@ -20,3 +20,27 @@ class LibraryDetailView(LoginRequiredMixin, DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.object.books.all()
         return context
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'logout.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
