5529fece1668403cdf34a771ee860383cd4d8bc5
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-02-25 15:04:35 +0200

    authenication

--- django-models/LibraryProject/relationship_app/views copy.py
+++ django-models/LibraryProject/relationship_app/views copy.py
@@ -2,17 +2,14 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Library
 from .models import Book, Author, Librarian
 
-@login_required
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-class LibraryDetailView(LoginRequiredMixin, DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
 
