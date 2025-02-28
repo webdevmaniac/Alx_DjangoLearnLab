32c59404556f8c90e798111fd644f5668f28edc3
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-02-28 15:19:39 +0200

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm
+from .forms import BookForm, ExampleForm
 from django.db.models import Q
 
 @permission_required('bookshelf.can_view_book', raise_exception=True)
@@ -46,3 +46,13 @@ def search_books(request):
         Q(title__icontains=query) | Q(author__icontains=query)
     )
     return render(request, 'search_results.html', {'books': books})
+
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            pass
+    else:
+        form = ExampleForm()
+    return render(request, 'example.html', {'form': form})
