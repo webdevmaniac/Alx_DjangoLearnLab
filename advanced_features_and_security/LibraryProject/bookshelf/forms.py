093341a378d055b6b75603ae86376eb5b9ca5287
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-02-28 11:33:09 +0200

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -6,3 +6,11 @@ class BookForm(forms.ModelForm):
         model = Book
         fields = ('title', 'author', 'publication_year')
 
+class ExampleForm(forms.Form):
+    name = forms.CharField(label='Your name', max_length=100)
+    email = forms.EmailField(label='Your email', max_length=100)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['placeholder'] = 'Enter your name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter your email'
