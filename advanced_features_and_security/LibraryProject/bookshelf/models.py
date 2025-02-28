6f230e8ecb6d5fac210c1363b2122a9a0e90ca13
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-02-28 10:11:57 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -37,3 +37,19 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ('can_view_book', 'Can view book'),
+            ('can_create_book', 'Can create book'),
+            ('can_edit_book', 'Can edit book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
+
+    def __str__(self):
+        return self.title
