73805bfc0b398f13ccd178790b6ea41d92b489aa
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-02-28 15:42:55 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -81,27 +81,26 @@ def register_view(request):
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
-def admin_view(request):
-    return render(request, 'admin_view.html')
-
-def librarian_view(request):
-    return render(request, 'librarian_view.html')
-
-def member_view(request):
-    return render(request, 'member_view.html')
-
 def is_admin(user):
     return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
 
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
 
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
 def is_member(user):
     return user.userprofile.role == 'Member'
 
-admin_view = user_passes_test(is_admin)(admin_view)
-librarian_view = user_passes_test(is_librarian)(librarian_view)
-member_view = user_passes_test(is_member)(member_view)
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
 
 def register(request):
     if request.method == 'POST':
