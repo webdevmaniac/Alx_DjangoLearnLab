8c3c24c9de446d741b1c481a24a883bc76776b8f
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-02-28 11:01:12 +0200

    we

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import BookForm
+from django.db.models import Q
 
 @permission_required('bookshelf.can_view_book', raise_exception=True)
 def book_list(request):
@@ -29,3 +31,48 @@ def delete_book(request, pk):
         # Delete book logic here
         pass
     return render(request, 'delete_book.html', {'book': book})
+
+
+@permission_required('bookshelf.can_view_book', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create_book', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'create_book.html', {'form': form})
+
+@permission_required('bookshelf.can_edit_book', raise_exception=True)
+def edit_book(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'edit_book.html', {'form': form})
+
+@permission_required('bookshelf.can_delete_book', raise_exception=True)
+def delete_book(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'delete_book.html', {'book': book})
+
+def search_books(request):
+    query = request.GET.get('q')
+    books = Book.objects.filter(
+        Q(title__icontains=query) | Q(author__icontains=query)
+    )
+    return render(request, 'search_results.html', {'books': books})
+
