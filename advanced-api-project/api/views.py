21d20ea7df0444de2e3886cc59bce62df3bda4fa
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-03-16 19:39:42 +0200

    qa

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,10 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly as AuthReadOnly
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
 
 # Create your views here.
 class BookListView(generics.ListAPIView):
@@ -52,3 +56,28 @@ class BookDeleteView(generics.DestroyAPIView):
 
     def get_queryset(self):
         return Book.objects.all()
+
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author', 'publication_year']
+
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author']
+
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
+
