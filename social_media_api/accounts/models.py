1f6bdabf5b1a8ccc165707f46c9763596bde136e
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-03-29 13:07:30 +0200

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False, blank=True)
+
+    def __str__(self):
+        return self.username
