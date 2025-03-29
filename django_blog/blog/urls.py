d5d12562780d2e30c0a02d060f5246be56e01966
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-03-29 11:59:03 +0200

    Create urls.py

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile_view, name='profile'),
+]
