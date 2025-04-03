c0f24b6e2ed5d423bc4b86b5fb233512a2897a79
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-04-03 19:12:53 +0200

    qw

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from django.contrib import admin
-from django.urls import include, path
 
 router = DefaultRouter()
 router.register(r'posts', views.PostViewSet, basename='post')
@@ -10,9 +8,5 @@ router.register(r'posts/(?P<post_id>\d+)/comments', views.CommentViewSet, basena
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-    path("api/", include("posts.urls")),
 ]
-
-
 
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -3,10 +3,16 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path
 from . import views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('admin/', admin.site.urls),
+    path('api/', include('posts.urls')),
 ]
+
+
+
