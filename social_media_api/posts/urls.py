2133b9f6afb2094c8865ec8b250ed4ae3036f65f
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2025-04-03 19:09:47 +0200

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
 from django.contrib import admin
 from django.urls import include, path
 
+router = DefaultRouter()
+router.register(r'posts', views.PostViewSet, basename='post')
+router.register(r'posts/(?P<post_id>\d+)/comments', views.CommentViewSet, basename='comment')
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api/', include('posts.urls')),
+    path("api/", include("posts.urls")),
 ]
+
+
+
